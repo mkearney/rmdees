05300011b390b20d2d2964a5a4091233514abcd9
Michael W. Kearney <kearneymw@missouri.edu>
2018-09-04 19:24:23 -0500

    initial

---
+++ R/title-icons.R
@@ -0,0 +1,41 @@
+
+#' Generate text for title icons
+#'
+#' Outputs Twitter and Github icons for Xaringan title slide
+#'
+#' @param twitter Screen name
+#' @param github User name
+#' @return String of html code.
+#' @export
+title_icons <- function(twitter = NULL, github = NULL) {
+  tw1 <- ""
+  tw2 <- ""
+  gh1 <- ""
+  gh2 <- ""
+  if (!is.null(twitter)) {
+    tw1 <- tfse::psub('<th style=\"padding-right:75px!important\">
+      <a href=\"https://twitter.com/{twitter}\"> <i class=\"fa fa-twitter\"></i> </a></th>',
+    	twitter)
+    tw2 <- tfse::psub('<th style=\"padding-right:75px!important\">
+      <a href=\"https://twitter.com/{twitter}\"> @{twitter} </a></th>',
+    	twitter, twitter)
+  }
+  if (!is.null(github)) {
+    gh1 <- tfse::psub('<th style=\"padding-left:75px!important\">
+      <a href=\"https://github.com/{github}\"> <i class=\"fa fa-github\"></i> </a></th>',
+    	github)
+    gh2 <- tfse::psub('<th style=\"padding-left:75px!important\">
+      <a href=\"https://github.com/{github}\"> @{github} </a></th>',
+    	github, github)
+  }
+  if (!is.null(tw1) | !is.null(gh1)) {
+    tr1 <- paste0('<tr>', tw1, gh1, '</tr>')
+    tr2 <- paste0('<tr style=\"background-color:#fff\">', tw2, gh2, '</tr>')
+  } else {
+    tr1 <- ""
+    tr2 <- ""
+  }
+  h <- paste0('<table style=\"border-style:none;padding-top:30px;\" class=\".table\">',
+    tr1, tr2, '</table>')
+  gsub("\n", "", h)
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# rmdees
+
+Useful functions for working with Rmd (rmarkdown) files.
